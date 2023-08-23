50903e93bf544efc4db0a15d2624d32707c69e9e
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2023-08-23 10:25:09 -0500

    Extracted mocks from initial test

---
+++ __mocks__/react-dnd.ts
@@ -0,0 +1,2 @@
+export const useDrag = () => [{ isDragging: false }, jest.fn()];
+export const useDrop = () => [{ isDragging: false }, jest.fn()]
--- src/templates/Home/index.test.tsx
+++ src/templates/Home/index.test.tsx
@@ -1,18 +1,6 @@
 import { render } from '@testing-library/react'
 import { Home } from "."
 
-jest.mock('next/navigation', () => ({
-  useRouter: () => ({
-    push: jest.fn()
-  })
-}));
-
-jest.mock('react-dnd', () => ({
-  useDrag: () => [{ isDragging: false }, jest.fn()],
-  useDrop: () => [{ isDragging: false }, jest.fn()]
-}));
-
-
 it('should render', () => {
   expect(true).toBe(true)
   const { container } = render(<Home />)
