44cc2086a1bdf1919834b1b61eee398ee43360a8
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2023-09-13 11:02:19 -0500

    Fixed initial config

--- src/model/CassandraClient.ts
+++ src/model/CassandraClient.ts
@@ -1,11 +1,13 @@
-import cassandra from "cassandra-driver";
+import { Client } from "cassandra-driver";
 
-const contactPoints = (process.env.CASSANDRA_HOSTS ?? "localhost").split(",");
+const contactPoints = (process.env.CASSANDRA_HOSTS ?? "127.0.0.1").split(",");
 const keyspace = process.env.CASSANDRA_KEYSPACE ?? "tasks";
-const localDataCenter = process.env.CASSANDRA_LOCAL_DATACENTER ?? "localhost";
+const localDataCenter = process.env.CASSANDRA_LOCAL_DATACENTER ?? "local1";
 
-export const client = new cassandra.Client({
+export const client = new Client({
   contactPoints,
   localDataCenter,
   keyspace,
 });
+
+client.connect();
