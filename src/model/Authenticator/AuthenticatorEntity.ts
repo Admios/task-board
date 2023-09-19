96c7255788c93900a6695f63374ffc22cd75cb48
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2023-09-19 10:12:17 -0500

    Moved comments to the DTO object

--- src/model/Authenticator/AuthenticatorEntity.ts
+++ src/model/Authenticator/AuthenticatorEntity.ts
@@ -1,18 +1,10 @@
 export interface AuthenticatorEntity {
-  // SQL: Encode to base64url then store as `TEXT`. Index this column
   id: string;
-  // SQL: Store raw bytes as `BYTEA`/`BLOB`/etc...
   credentialPublicKey: string;
-  // SQL: Consider `BIGINT` since some authenticators return atomic timestamps as counters
   counter: number;
-  // SQL: `VARCHAR(32)` or similar, longest possible value is currently 12 characters
-  // Ex: 'singleDevice' | 'multiDevice'
   credentialDeviceType: string;
-  // SQL: `BOOL` or whatever similar type is supported
   credentialBackedUp: boolean;
-  // SQL: `VARCHAR(255)` and store string array as a CSV string
-  // Ex: ['usb' | 'ble' | 'nfc' | 'internal']
   transports?: string;
-  // Foreign Key to a user model
+
   userId: string;
 }
