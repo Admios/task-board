36c4e92a04ddc9259730e98e911328e0713b82eb
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2023-09-28 09:26:44 -0500

    Added Multiple Test Users

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -14,71 +14,76 @@ console.log("Using keyspace: ", process.env.CASSANDRA_KEYSPACE);
 /****
  * SEED DATA
  */
-const demo_username = "TEST";
-type ColumnSeed = Omit<ColumnDTO, "id"> & { taskNames: string[] };
+const owners = ["test1", "test2", "test3"];
+type ColumnSeed = {
+  name: ColumnDTO["name"];
+  position: ColumnDTO["position"];
+  color: ColumnDTO["color"];
+  taskNames: string[];
+};
 const columns: ColumnSeed[] = [
   {
     name: "New",
     position: 0,
     color: "black",
     taskNames: ["Task 17", "Task 28", "Task 39"],
-    owner: demo_username,
   },
   {
     name: "In Progress",
     position: 1,
     color: "orange",
     taskNames: ["Task 45", "Task 56"],
-    owner: demo_username,
   },
   {
     name: "In Review",
     position: 2,
     color: "green",
     taskNames: ["Task 87", "Task 29", "Task 63", "Task 4"],
-    owner: demo_username,
   },
   {
     name: "Done",
     position: 3,
     color: "blue",
     taskNames: ["Task 7", "Task 8", "Task 9"],
-    owner: demo_username,
   },
 ];
 
 const columnRepository = new ColumnRepository();
 const taskRepository = new TaskRepository();
 
+async function createColumn(column: ColumnSeed, owner: string) {
+  const columnId = uuid();
+  await columnRepository.create({
+    id: columnId,
+    name: column.name,
+    position: column.position,
+    color: column.color,
+    owner,
+  });
+
+  const taskPromises = column.taskNames.map(async (taskName, index) =>
+    taskRepository.create({
+      id: uuid(),
+      text: `${taskName} (${owner})`,
+      columnId,
+      position: index,
+      owner,
+    }),
+  );
+
+  await Promise.all(taskPromises);
+  console.log(`Created column ${column.name} for user ${owner}`);
+}
+
 async function execute() {
   console.log("Start seeding");
   await client.connect();
 
-  const columnPromises = columns.map(async (column) => {
-    const columnId = uuid();
-    await columnRepository.create({
-      id: columnId,
-      name: column.name,
-      position: column.position,
-      color: column.color,
-      owner: column.owner,
-    });
+  const promises = owners
+    .map((owner) => columns.map((column) => createColumn(column, owner)))
+    .flat(1);
 
-    const taskPromises = column.taskNames.map(async (taskName, index) =>
-      taskRepository.create({
-        id: uuid(),
-        text: taskName,
-        columnId,
-        position: index,
-        owner: column.owner,
-      }),
-    );
-
-    await Promise.all(taskPromises);
-    console.log(`Created column ${column.name}`);
-  });
-
-  await Promise.all(columnPromises);
+  await Promise.all(promises);
   await client.shutdown();
 }
 execute();
