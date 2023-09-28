f81528393ca895dd2282bb4ac4580ed5aabd2f4f
Roberto Eduardo Zubieta <zubieta.roberto.e@gmail.com>
2023-09-28 10:23:24 -0500

    Task Owners (#39)
    Added Owner Field to tasks and columns

--- src/model/Column/ColumnRepository.ts
+++ src/model/Column/ColumnRepository.ts
@@ -9,4 +9,13 @@ export class ColumnRepository extends BaseRepository<ColumnDTO> {
   public get entityName() {
     return "Column";
   }
+
+  async listByUserId(userId: string) {
+    const query = this.mapper.mapWithQuery(
+      `SELECT * FROM ${this.tableName} WHERE owner = ?`,
+      (doc: { id: string }) => [doc.id],
+    );
+    const result = await query({ id: userId });
+    return result.toArray();
+  }
 }
