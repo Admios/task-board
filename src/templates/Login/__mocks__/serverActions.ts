545a54e6a3f4c0668ee0e36cea5725e611f53070
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2023-10-11 13:53:25 -0500

    Fixed Login Tests

--- src/templates/Login/__mocks__/serverActions.ts
+++ src/templates/Login/__mocks__/serverActions.ts
@@ -1,12 +1,6 @@
-export const generateRegistrationOptions = jest.fn().mockResolvedValue({
+export const generateOptions = jest.fn().mockResolvedValue({
   options: {},
 });
-export const generateAuthenticationOptions = jest.fn().mockResolvedValue({
-  options: {},
-});
-export const verifyRegistration = jest.fn().mockResolvedValue({
-  verification: { verified: true },
-});
-export const verifyAuthentication = jest.fn().mockResolvedValue({
-  verification: { verified: true },
+export const verifyOptions = jest.fn().mockResolvedValue({
+  verified: true,
 });
