b3e139ff83cfc1b596ac317fc4e0bd21d87cd3c9
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2023-10-19 12:11:56 -0500

    Added button to create "Default Columns"

--- src/templates/KanbanBoard/KanbanColumnList.tsx
+++ src/templates/KanbanBoard/KanbanColumnList.tsx
@@ -1,3 +1,4 @@
+import { StateDTO } from "@/model/State";
 import {
   Button,
   Card,
@@ -9,8 +10,65 @@ import {
   Heading,
   Text,
 } from "@chakra-ui/react";
+import { v4 as uuid } from "uuid";
 import { KanbanColumn } from "./KanbanColumn";
+import { addStateDB } from "./kanbanActions";
 import { Task, useZustand } from "./model";
+
+type StateSeed = {
+  name: StateDTO["name"];
+  position: StateDTO["position"];
+  color: StateDTO["color"];
+};
+
+const DEFAULT_STATES: StateSeed[] = [
+  {
+    name: "New",
+    position: 0,
+    color: "black",
+  },
+  {
+    name: "In Progress",
+    position: 1,
+    color: "orange",
+  },
+  {
+    name: "In Review",
+    position: 2,
+    color: "green",
+  },
+  {
+    name: "Done",
+    position: 3,
+    color: "blue",
+  },
+];
+
+async function createDefaultStates() {
+  const { user, statesOrder, addState } = useZustand.getState();
+
+  if (!user) {
+    return;
+  }
+
+  if (statesOrder.length) {
+    return;
+  }
+
+  const promises = DEFAULT_STATES.map(async (state) => {
+    const item: StateDTO = {
+      id: uuid(),
+      name: state.name,
+      color: state.color,
+      position: state.position,
+      owner: user.email,
+    };
+    addState(item);
+    await addStateDB(item);
+  });
+
+  await Promise.all(promises);
+}
 
 interface KanbanColumnListProps {
   isStateDialogOpen: boolean;
@@ -37,8 +95,8 @@ export function KanbanColumnList({
 
           <CardBody>
             <Text>
-              You have no content yet. Create your first column with the button
-              below!
+              You have no content yet. Create your first column, or create our
+              default layout!
             </Text>
           </CardBody>
 
@@ -49,6 +107,13 @@ export function KanbanColumnList({
               disabled={isStateDialogOpen}
             >
               Create Column
+            </Button>
+            <Button
+              colorScheme="blue"
+              onClick={createDefaultStates}
+              disabled={isStateDialogOpen}
+            >
+              Create Default Layout
             </Button>
           </CardFooter>
         </Card>
