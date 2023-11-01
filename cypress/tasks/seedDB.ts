f6c70a627ed4c037a28e4de2bacea19773274170
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2023-11-01 13:59:53 -0500

    Seeds working correctly

--- cypress/tasks/seedDB.ts
+++ cypress/tasks/seedDB.ts
@@ -1,11 +1,9 @@
-import { client } from "@/model/CassandraClient";
 import { StateDTO, StateRepository } from "@/model/State";
 import { TaskRepository } from "@/model/Task";
-import { UserRepository } from "@/model/User";
-import { loadEnvConfig } from "@next/env";
+import { UserDTO } from "@/model/User";
 import { v4 as uuid } from "uuid";
-
-const result = loadEnvConfig("./");
+import stateSeed from "../fixtures/stateSeed.json";
+import users from "../fixtures/users.json";
 
 /****
  * SEED DATA
@@ -16,34 +14,7 @@ type StateSeed = {
   color: StateDTO["color"];
   taskNames: string[];
 };
-const states: StateSeed[] = [
-  {
-    name: "New",
-    position: 0,
-    color: "black",
-    taskNames: ["Task 17", "Task 28", "Task 39"],
-  },
-  {
-    name: "In Progress",
-    position: 1,
-    color: "orange",
-    taskNames: ["Task 45", "Task 56"],
-  },
-  {
-    name: "In Review",
-    position: 2,
-    color: "green",
-    taskNames: ["Task 87", "Task 29", "Task 63", "Task 4"],
-  },
-  {
-    name: "Done",
-    position: 3,
-    color: "blue",
-    taskNames: ["Task 7", "Task 8", "Task 9"],
-  },
-];
 
-const userRepository = new UserRepository();
 const stateRepository = new StateRepository();
 const taskRepository = new TaskRepository();
 
@@ -71,13 +42,8 @@ async function createState(state: StateSeed, owner: string) {
 }
 
 export default async function seedDB() {
-  const owners = await userRepository.list();
-  if (owners.length < 0) {
-    return;
-  }
-
-  const promises = owners
-    .splice(0, 5) // Take 5 users. Not necessarily the first 5.
+  const states = stateSeed as StateSeed[];
+  const promises = (users as UserDTO[])
     .map((owner) => states.map((state) => createState(state, owner.email)))
     .flat(1);
 
