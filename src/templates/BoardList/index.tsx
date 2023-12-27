355995f92495b6ee37311ff19e2bdfb09b0285c6
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2023-12-27 11:43:13 -0500

    Using a Bulma Panel for the board list

--- src/templates/BoardList/index.tsx
+++ src/templates/BoardList/index.tsx
@@ -3,6 +3,7 @@
 import { Navbar } from "@/components/Navbar";
 import { BoardDTO } from "@/model/Board";
 import { UserDTO } from "@/model/User";
+import { ChartBarIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import styles from "./index.module.scss";
 
@@ -17,12 +18,16 @@ export async function BoardList({ boards, user }: BoardListProps) {
       <Navbar user={user} navbarItems={[]} />
 
       <section className="container">
-        <h1 className="title">Your Boards</h1>
-        <ul>
-          <li>
-            <Link href={`/b/1`}>Test Board</Link>
-          </li>
-        </ul>
+        <nav className="panel is-primary">
+          <p className="panel-heading">Your Boards</p>
+
+          <Link className="panel-block is-active" href={`/b/1`}>
+            <span className="panel-icon">
+              <ChartBarIcon />
+            </span>
+            Test Board
+          </Link>
+        </nav>
       </section>
     </main>
   );
