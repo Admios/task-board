b0c988c058aa2f6fda02cf4919d2d5fb0bfe0613
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2024-01-11 10:25:23 -0500

    All tasks of a single board in a single query

--- src/model/Task/TaskRepository.ts
+++ src/model/Task/TaskRepository.ts
@@ -18,4 +18,14 @@ export class TaskRepository extends BaseRepository<TaskDTO> {
     const result = await query({ id: stateId });
     return result.toArray();
   }
+
+  async listByStateIdList(stateIds: string[]) {
+    const masks = stateIds.map(() => "?").join(", ");
+    const query = this.mapper.mapWithQuery(
+      `SELECT * FROM ${this.tableName} WHERE state_id IN (${masks}) ALLOW FILTERING`,
+      () => stateIds,
+    );
+    const result = await query({});
+    return result.toArray();
+  }
 }
