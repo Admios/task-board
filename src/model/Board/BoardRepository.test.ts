fe84733620acb8c09f777abdc6a300f36587c434
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2024-01-12 13:10:46 -0500

    Fixed BoardRepository Tests

--- src/model/Board/BoardRepository.test.ts
+++ src/model/Board/BoardRepository.test.ts
@@ -4,44 +4,35 @@ import { mapper } from "../CassandraClient";
 
 jest.mock("../CassandraClient");
 
-describe("BoardRepository", () => {
-  afterEach(() => {
-    jest.clearAllMocks();
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+it("listByUserId should return a list of boards for a given user", async () => {
+  const userId = "user123";
+  const boards: BoardDTO[] = [
+    {
+      id: "boards1",
+      name: "Boards 1",
+      owner: userId,
+    },
+    {
+      id: "boards2",
+      name: "Boards 2",
+      owner: userId,
+    },
+  ];
+
+  const queryMock = jest.fn().mockResolvedValue({
+    toArray: () => boards,
+  });
+  (mapper.forModel as jest.Mock).mockReturnValue({
+    mapWithQuery: jest.fn().mockReturnValue(queryMock),
   });
 
-  it("listByUserId should return a list of boards for a given user", async () => {
-    const boardRepository: BoardRepository = new BoardRepository();
-    const mapperMock = mapper.forModel("");
-    const userId = "user123";
-    const boards: BoardDTO[] = [
-      {
-        id: "boards1",
-        name: "Boards 1",
-        owner: userId,
-      },
-      {
-        id: "boards2",
-        name: "Boards 2",
-        owner: userId,
-      },
-    ];
+  const boardRepository = new BoardRepository();
+  const result = await boardRepository.listByUserId(userId);
 
-    (mapperMock.mapWithQuery as jest.Mock).mockReturnValue(
-      async (params: { id: string }) => {
-        const response = boards.filter((board) => board.owner === params.id);
-
-        return {
-          toArray: () => response,
-        };
-      },
-    );
-
-    const result = await boardRepository.listByUserId(userId);
-
-    expect(result).toEqual(boards);
-    expect(mapperMock.mapWithQuery as jest.Mock).toHaveBeenCalledWith(
-      `SELECT * FROM boards WHERE owner = ?`,
-      expect.any(Function),
-    );
-  });
+  expect(result).toEqual(boards);
+  expect(queryMock).toHaveBeenCalledWith({ id: userId });
 });
